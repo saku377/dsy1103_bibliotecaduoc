fc9dfdea7f6c6b989c097bd8aac0bb2a2aa25b8d
you@example.com <you@example.com>
2026-03-27 17:00:11 -0300

    Version con validacion funcionando

--- src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
+++ src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.validation.BindingResult;
 
 import java.util.List;
 
@@ -31,10 +32,20 @@ public class LibroController {
         return libroService.getLibros();
     }
 
+    //@PostMapping
+    //public Libro agregarLibro(@Valid @RequestBody ClientRequest request) {
+    //    return libroService.saveLibro(LibroMapper.toModel(request));
+    //}
+
     @PostMapping
-    public Libro agregarLibro(@Valid @RequestBody ClientRequest request) {
+    public Object agregarLibro(@Valid @RequestBody ClientRequest request, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return result.getAllErrors();
+        }
+
         return libroService.saveLibro(LibroMapper.toModel(request));
-    }
+    }    
 
     @GetMapping("{id}")
     public Libro buscarLibro(@PathVariable int id){
